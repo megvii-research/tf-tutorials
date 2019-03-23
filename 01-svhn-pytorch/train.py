6865b5a92e98ae3893d8d4f6a7b445d838d42cff
niuyazhe <niuyazhe@sensetime.com>
2019-03-24 00:31:47 +0800

    add pytorch train

--- 01-svhn-pytorch/train.py
+++ 01-svhn-pytorch/train.py
@@ -2,169 +2,128 @@
 # -*- coding: utf-8 -*-
 # =======================================
 # File Name : train.py
-# Purpose : run a session for training
-# Creation Date : 2019-02-19 15:55
+# Purpose : build a compute graph
+# Creation Date : 2019-03-23 23:28
 # Last Modified :
-# Created By : sunpeiqin
+# Created By : niuyazhe
 # =======================================
+
 
 import os
 import argparse
-import tensorflow as tf
-
-from model import Model
-from dataset import Dataset
+import torch
+from torch.utils.data import DataLoader
+from torchvision import transforms
 from common import config
+from dataset import SvhnDataset
+from model import Model
+from loss import Softmax, LpNorm
 
-def get_dataset_batch(ds_name):
-    dataset = Dataset(ds_name)
-    ds_gnr = dataset.load().instance_generator
-    ds = tf.data.Dataset().from_generator(ds_gnr, output_types=(tf.float32, tf.int32),)
-    if ds_name == 'train':
-        ds = ds.shuffle(dataset.instances_per_epoch)
-        ds = ds.repeat(config.nr_epoch)
-    elif ds_name == 'test':
-        ds = ds.repeat(config.nr_epoch // config.test_interval)
-    ds = ds.batch(config.minibatch_size, drop_remainder=True)
-    ds_iter = ds.make_one_shot_iterator()
-    sample_gnr = ds_iter.get_next()
-    return sample_gnr, dataset
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--continue', dest='continue_path', required=False)
-    parser.add_argument('-l', '--loss', default='softmax')
-    args = parser.parse_args()
+def train(network_model, train_loader, test_loader, optimizer, scheduler, criterion, regularizer, args):
+    global_cnt = 0
+    for epoch in range(args.epoch):
+        scheduler.step()
+        for idx, data in enumerate(train_loader):
+            global_cnt += 1
+            img, label = data
+            if torch.cuda.is_available():
+                img, label = img.cuda(), label.cuda()
+            output = network_model(img)
 
-    assert args.loss in ['softmax', 'abs-max', 'square-max', 'plus-one-abs-max', 'non-negative-max']
+            loss = criterion(output, label)
+            if regularizer is not None:
+                reg = regularizer(network_model)
+                loss += reg
+            else:
+                reg = torch.Tensor([0.])
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
-    ## load dataset
-    train_batch_gnr, train_set = get_dataset_batch(ds_name='train')
-    test_batch_gnr, test_set = get_dataset_batch(ds_name='test')
+            if global_cnt % config.show_interval == 0:
+                accuracy = torch.eq(torch.max(output, dim=1), label).sum() / label.shape[0]
+                print(
+                    '[epoch:{}, batch:{}]\t'.format(epoch, idx),
+                    '[loss: {:.3f}]\t'.format(loss.item()),
+                    '[loss_reg: {:.3f}]\t'.format(reg.item()),
+                    '[accuracy: {:.3f}]\t'.format(accuracy.item()),
+                    '[lr: {:.3f}]\n'.format(scheduler.get_lr().item())
+                )
+        if epoch % config.test_interval == 0:
+            loss_sum = 0.
+            acc_sum = 0.
+            test_batch_num = 0
+            for idx, data in enumerate(test_loader):
+                test_batch_num += 1
+                img, label = data
+                if torch.cuda.is_available():
+                    img, label = img.cuda(), label.cuda()
+                output = network_model(img)
 
-    ## build graph
-    network = Model()
-    placeholders, label_onehot, logits = network.build()
+                loss = criterion(output, label)
+                loss_sum += loss_sum.item()
+                acc_sum += torch.eq(torch.max(output, dim=1), label).sum().item()
+            print('\n***************validation result*******************')
+            print(
+                'loss_avg: {:.3f}\t'.format(loss_sum / test_batch_num),
+                'accuracy_avg: {:.3f}\n'.format(acc_sum / test_batch_num)
+            )
+            print('****************************************************\n')
+
+        if epoch % config.snapshot_interval == 0:
+            torch.save(network_model.state_dict(), os.path.join(config.log_model_dir, 'epoch-{}'.format(epoch)))
+
+
+def test(network_model, test_loader):
+    raise NotImplementedError
+
+
+def main(args):
+    network_model = Model()
+    if torch.cuda.is_available():
+        network_model = network_model.cuda()
 
     if args.loss == 'softmax':
-        preds = tf.nn.softmax(logits)
-    elif args.loss == 'abs-max':
-        abs_logits = tf.abs(logits)
-        preds = abs_logits / tf.reduce_sum(abs_logits, axis=1, keepdims=True)
-    elif args.loss == 'square-max':
-        square_logits = logits * logits
-        preds = square_logits / tf.reduce_sum(square_logits, axis=1, keepdims=True)
-    elif args.loss == 'plus-one-abs-max':
-        plus_one_abs_logits = tf.abs(logits) + 1.0
-        preds = plus_one_abs_logits / tf.reduce_sum(plus_one_abs_logits, axis=1, keepdims=True)
-    elif args.loss == 'non-negative-max':
-        relu_logits = tf.nn.relu(logits)
-        preds = relu_logits / tf.reduce_sum(relu_logits, axis=1, keepdims=True)
+        criterion = Softmax()
     else:
-        raise("Invalid loss type")
+        raise ValueError
 
-    correct_pred = tf.equal(tf.cast(tf.argmax(preds, 1), dtype=tf.int32),
-                            tf.cast(tf.argmax(label_onehot, 1), dtype=tf.int32))
-    accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
-    loss_reg = tf.add_n(tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES))
-    loss = tf.losses.softmax_cross_entropy(label_onehot, logits) + loss_reg
+    if args.lp_norm == None:
+        regularizer = None
+    else:
+        raise ValueError
 
-    ## train config
-    global_steps = tf.Variable(0, trainable=False)
-    boundaries = [train_set.minibatchs_per_epoch*15, train_set.minibatchs_per_epoch*40]
-    values = [0.01, 0.001, 0.0005]
-    lr = tf.train.piecewise_constant(global_steps, boundaries, values)
-    #opt = tf.train.MomentumOptimizer(lr, momentum=0.9) # set optimizer
-    opt = tf.train.AdamOptimizer(lr)
-    # in order to update BN in every iter
-    update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
-    with tf.control_dependencies(update_ops):
-        train = opt.minimize(loss)
+    optimizer = torch.optim.Adam(network_model.parameters(), lr=args.lr)
+    scheduler = torch.optim.MultiStepLR(optimizer, args.lr_milestone, gamma=0.1)
 
-    ## init tensorboard
-    tf.summary.scalar('loss_regularization', loss_reg)
-    tf.summary.scalar('loss_crossEntropy', loss - loss_reg)
-    tf.summary.scalar('loss', loss)
-    tf.summary.scalar('accuracy', accuracy)
-    tf.summary.scalar('learning_rate', lr)
-    merged = tf.summary.merge_all()
-    train_writer = tf.summary.FileWriter(os.path.join(config.log_dir, 'tf_log', 'train'),
-                                         tf.get_default_graph())
-    test_writer = tf.summary.FileWriter(os.path.join(config.log_dir, 'tf_log', 'test'),
-                                        tf.get_default_graph())
+    transform = transforms.Compose([
+            transforms.ToTensor(),
+        ])
+    train_set = SvhnDataset(root=args.root, train=True, transform=transform)
+    test_set = SvhnDataset(root=args.root, train=False, transform=transform)
+    train_loader = DataLoader(train_set, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers, pin_memory=True)
+    test_loader = DataLoader(test_set, batch_size=args.batch_size, shuffle=False, num_workers=args.num_workers, pin_memory=True)
 
-    ## create a session
-    tf.set_random_seed(12345) # ensure consistent results
-    global_cnt = 0
-    epoch_start = 0
-    g_list = tf.global_variables()
-    saver = tf.train.Saver(var_list=g_list)
-    with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer()) # init all variables
-        if args.continue_path: # load a model snapshot
-            ckpt = tf.train.get_checkpoint_state(args.continue_path)
-            saver.restore(sess, ckpt.model_checkpoint_path)
-            epoch_start = int(ckpt.model_checkpoint_path.split('/')[-1].split('-')[1])
-            global_cnt = epoch_start * train_set.minibatchs_per_epoch
+    if arg.evaluate:
+        test(network_model, test_loader)
+        return
 
-        ## training
-        for epoch in range(epoch_start+1, config.nr_epoch+1):
-            for _ in range(train_set.minibatchs_per_epoch):
-                global_cnt += 1
-                images, labels = sess.run(train_batch_gnr)
-                feed_dict = {
-                    placeholders['data']: images,
-                    placeholders['label']: labels,
-                    global_steps: global_cnt,
-                    placeholders['is_training']: True,
-                }
-                _, loss_v, loss_reg_v, acc_v, lr_v, summary = sess.run([train, loss, loss_reg,
-                                                                        accuracy, lr, merged],
-                                                                       feed_dict=feed_dict)
-                if global_cnt % config.show_interval == 0:
-                    train_writer.add_summary(summary, global_cnt)
-                    print(
-                        "e:{},{}/{}".format(epoch, global_cnt % train_set.minibatchs_per_epoch,
-                                            train_set.minibatchs_per_epoch),
-                        'loss: {:.3f}'.format(loss_v),
-                        'loss_reg: {:.3f}'.format(loss_reg_v),
-                        'acc: {:.3f}'.format(acc_v),
-                        'lr: {:.3f}'.format(lr_v),
-                    )
+    train(network_model, train_loader, test_loader, optimizer, scheduler, criterion, regularizer, args)
 
-            ## validation
-            if epoch % config.test_interval == 0:
-                loss_sum = 0
-                acc_sum = 0
-                for i in range(test_set.minibatchs_per_epoch):
-                    images, labels = sess.run(test_batch_gnr)
-                    feed_dict = {
-                        placeholders['data']: images,
-                        placeholders['label']: labels,
-                        global_steps: global_cnt,
-                        placeholders['is_training']: False
-                    }
-                    loss_v, acc_v, summary = sess.run([loss, accuracy, merged],
-                                                      feed_dict=feed_dict)
-                    loss_sum += loss_v
-                    acc_sum += acc_v
-                test_writer.add_summary(summary, global_cnt)
-                print("\n**************Validation results****************")
-                print('loss_avg: {:.3f}'.format(loss_sum/test_set.minibatchs_per_epoch),
-                      'accuracy_avg: {:.3f}'.format(acc_sum/test_set.minibatchs_per_epoch))
-                print("************************************************\n")
 
-            ## save model
-            if epoch % config.snapshot_interval == 0:
-                saver.save(sess, os.path.join(config.log_model_dir, 'epoch-{}'.format(epoch)),
-                           global_step=global_cnt)
-
-        print('Training is done, exit.')
-
+'''
 if __name__ == "__main__":
-    try:
-        main()
-    except KeyboardInterrupt:
-        os._exit(1)
-
-# vim: ts=4 sw=4 sts=4 expandtab
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--root', default='./')
+    parser.add_argument('--batch_size', default=256)
+    parser.add_argument('--num_workers', default=4)
+    parser.add_argument('--loss', default=None)
+    parser.add_argument('--lp_norm', default=None)
+    parser.add_argument('--lr', default=0.01)
+    parser.add_argument('--lr_milestone', default=[15, 40])
+    parser.add_argument('--epoch', default=60)
+    parser.add_argument('--evaluate', default=False)
+    args = parser.parse_args()
+    main(args)
+'''
