214bdb1732895fdb60d6320bbaa4144b0ee47a9e
niuyazhe <niuyazhe@sensetime.com>
2019-04-11 00:57:21 +0800

    add lr scheduler

--- 02-feature-inversion-pytorch/code/train.py
+++ 02-feature-inversion-pytorch/code/train.py
@@ -39,6 +39,7 @@ def output_img(img, name):
 
 def extract_feature(model, inputs, layer_str, transform_dict):
     feature_map = torch.Tensor().cuda()
+
     def hook(module, input_feature, output_feature):
         feature_map.resize_(output_feature.shape)
         feature_map.copy_(output_feature)
@@ -63,7 +64,10 @@ class FeatureMatchLpLoss(nn.Module):
 
 def train(args):
     log_dir = os.path.join(args.output_dir, "M{}_L{}_W{}_F{}".format(
-        args.model, args.lr, args.weight_tv, "-".join(args.feature)
+        args.model,
+        str(args.lr)+"-".join([str(x) for x in args.lr_milestones]),
+        args.weight_tv,
+        "-".join(args.feature)
     ))
     if not os.path.exists(log_dir):
         os.mkdir(log_dir)
@@ -82,7 +86,8 @@ def train(args):
     model.eval()
 
     img = load_img(args.img_path, args.img_shape)
-    pre_noise = np.random.uniform(low=-3, high=3, size=img.shape).astype(np.float32)
+    pre_noise = np.random.uniform(
+        low=-3, high=3, size=img.shape).astype(np.float32)
     pre_noise = sigmoid(pre_noise)
     img_tensor = torch.from_numpy(img).permute(
         2, 0, 1).contiguous().unsqueeze(0).cuda()
@@ -93,13 +98,17 @@ def train(args):
     criterion = FeatureMatchLpLoss(p=2)
 
     optimizer = Adam([noise_tensor], lr=args.lr)
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(
+        optimizer, milestones=args.lr_milestones, gamma=0.5)
 
     for epoch in range(args.epoch):
-
+        scheduler.step()
         loss = torch.Tensor().cuda()
         for item in args.feature:
-            img_output = extract_feature(model, img_tensor, item, transform_dict)
-            noise_output = extract_feature(model, noise_tensor, item, transform_dict)
+            img_output = extract_feature(
+                model, img_tensor, item, transform_dict)
+            noise_output = extract_feature(
+                model, noise_tensor, item, transform_dict)
             loss += criterion(noise_output, img_output)
 
         loss = criterion(img_output, noise_output)
@@ -111,7 +120,8 @@ def train(args):
             print("e:{}---loss:{:.5f}".format(epoch, loss.item()))
             print("e:{}---loss:{:.5f}".format(epoch, loss.item()), file=log_file)
         if epoch % args.save_interval == 0:
-            noise_np = noise_tensor.data.cpu().squeeze(0).permute(1, 2, 0).contiguous().numpy()
+            noise_np = noise_tensor.data.cpu().squeeze(
+                0).permute(1, 2, 0).contiguous().numpy()
             output_img(noise_np, os.path.join(
                 log_dir, "epoch_{}.png".format(epoch)))
     log_file.close()
@@ -124,9 +134,10 @@ if __name__ == "__main__":
     parser.add_argument('--img_path', default='../images/face.jpg')
     parser.add_argument('--img_shape', default=(224, 224))
     parser.add_argument('--lr', default=1e-3)
+    parser.add_argument('--lr_milestones', default=[])
     parser.add_argument('--weight_tv', default=0)
     parser.add_argument('--feature', default=['conv3_1'])
-    parser.add_argument('--epoch', default=4000)
+    parser.add_argument('--epoch', default=2000)
     parser.add_argument('--save_interval', default=10)
     parser.add_argument('--show_interval', default=10)
     args = parser.parse_args()
